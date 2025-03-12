eca4d0a7f337b62af3b1814f4361df6638b60e47
anfal47 / <muhammedanfal845@gmail.com>
2025-03-12 23:01:45 +0530

    banner controller changed

--- lib/features/shop/controllers/banner_controller.dart
+++ lib/features/shop/controllers/banner_controller.dart
@@ -19,22 +19,10 @@ class BannerController extends GetxController {
   //-- load category data
   Future<void> fetchBanners() async {
     try {
-      // //-- show loader while loading categories
+      //-- show loader while loading categories
       // isLoading.value = true;
 
-      // //-- fetch categories from firebase
-      // final category = await categoryRepository.getCategories();
 
-      // //-- update the category list
-      // allcategories.assignAll(category);
-
-      // //-- filter the featured category
-
-      // final featured = category.where((category) => category.isFeatured == true && category.parentId.isEmpty).take(8).toList();
-      // featuredcategories.assignAll(featured);
-
-      // allcategories.refresh();
-      // featuredcategories.refresh();
     } catch (e) {
       Loaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
     } finally {
